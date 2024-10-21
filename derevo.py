a6b248fd64ddaf079065a452a6aa5cd331c3a21c
max6971 <cmytt@mail.ru>
2024-10-21 21:42:26 +0700

    Create derevo.py

---
+++ derevo.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if not self.root:
+            self.root = new_node
+        else:
+            current = self.root
+            while True:
+                if value < current.value:
+                    if current.left is None:
+                        current.left = new_node
+                        break
+                    current = current.left
+                else:
+                    if current.right is None:
+                        current.right = new_node
+                        break
+                    current = current.right
+
+    def search(self, value):
+        current = self.root
+        while current:
+            if value == current.value:
+                return True
+            elif value < current.value:
+                current = current.left
+            else:
+                current = current.right
+        return False
+
+    def inorder_traversal(self, node, visited=None):
+        if visited is None:
+            visited = []
+        if node:
+            self.inorder_traversal(node.left, visited)
+            visited.append(node.value)
+            self.inorder_traversal(node.right, visited)
+        return visited
+
+
+bst = BinarySearchTree()
+bst.insert(50)
+bst.insert(30)
+bst.insert(70)
+bst.insert(20)
+bst.insert(40)
+bst.insert(60)
+bst.insert(80)
+
+print("Обход по порядку:", bst.inorder_traversal(bst.root))
+
+
+print("Поиск 40:", bst.search(40))
+print("Поиск 90:", bst.search(90))
